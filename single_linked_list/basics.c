a4e8f9be63ec2e81d36683536eb5e30bcec2a7f2
Yogeshkumar1411 <syogeshkumar1411@gmail.com>
2024-01-09 12:29:20 +0530

    basic operation in linked list

--- single_linked_list/basics.c
+++ single_linked_list/basics.c
@@ -11,6 +11,7 @@ struct node
 	struct node *link;
 };
 
+/*program to insert a node at end*/
 struct node *insert_at_end(struct node *ptr,int data)
 {
 	struct node *temp = malloc(sizeof(struct node));
@@ -34,7 +35,7 @@ struct node *insert_at_end(struct node *ptr,int data)
 	return ptr;
 }
 
-
+/*program to print the nodes*/
 void print(struct node *ptr)
 {
 	struct node *p = ptr;
@@ -45,6 +46,7 @@ void print(struct node *ptr)
 	}	
 }
 
+/*program to delete the node at end*/
 struct node *delete_at_end(struct node *ptr)
 {
 	struct node *p = ptr;
@@ -59,7 +61,7 @@ struct node *delete_at_end(struct node *ptr)
 	return p;
 }
 
-
+/*program to insert node at front*/
 struct node *insert_at_front(struct node *ptr,int data)
 {
 	struct node *temp = malloc(sizeof(struct node));
@@ -69,7 +71,7 @@ struct node *insert_at_front(struct node *ptr,int data)
 	return ptr;
 }
 
-
+/*program to delete node at front*/
 struct node *delete_at_front(struct node *ptr)
 {
 	struct node *temp = ptr;
@@ -79,6 +81,7 @@ struct node *delete_at_front(struct node *ptr)
 	return ptr;
 }
 
+/*Find Length of a Linked List using Loop*/
 int find_length(struct node *ptr)
 {
 	int count = 0;
@@ -91,7 +94,7 @@ int find_length(struct node *ptr)
 	return count;
 }
 
-
+/*Find Length of a Linked List using Recursion*/
 int find_length_rec(struct node *ptr)
 {	
 //	printf("temp data = %d\n",temp->data);
@@ -105,7 +108,7 @@ int find_length_rec(struct node *ptr)
 	}
 }
 
-
+/*Program to insert the node after given node*/
 struct node *insert_after_node(struct node *ptr, int data, int node)
 {
 	struct node *temp = malloc(sizeof(struct node));
@@ -123,7 +126,7 @@ struct node *insert_after_node(struct node *ptr, int data, int node)
 	return ptr;
 }
 
-
+/*Program to delete the node after given node*/
 struct node *delete_node_after(struct node *ptr, int node)
 {
 	struct node *temp = ptr;
@@ -137,7 +140,7 @@ struct node *delete_node_after(struct node *ptr, int node)
 	
 }
 
-
+/*Program to Reverse a Linked List using Loop*/
 struct node *reverse_loop(struct node *ptr,struct node *next,struct node *prev)
 {
 	//struct node *temp = ptr;
@@ -153,6 +156,7 @@ struct node *reverse_loop(struct node *ptr,struct node *next,struct node *prev)
 	return ptr;
 }
 
+/*Program to Reverse a Linked List using Recursion*/
 struct node *reverse_rec(struct node *ptr,struct node *next,struct node *prev)
 {	
 	if(ptr==NULL)
@@ -169,6 +173,8 @@ struct node *reverse_rec(struct node *ptr,struct node *next,struct node *prev)
 	}
 }
 
+
+/*Print a Linked List in Reverse Order using Recursion*/
 void recursion_print(struct node *ptr)
 {
 	if(ptr==NULL)
@@ -182,13 +188,100 @@ void recursion_print(struct node *ptr)
 	}
 }
 
+/*Program to Print Alternate Nodes of Linked List*/
+void print_alternate(struct node *ptr)
+{
+	while(ptr)
+	{
+		printf("alter data = %d\n",ptr->data);
+		ptr = ptr->link->link;
+	}
+}
+
+/*Program to Find Nth Node from the End of a Linked List*/
+void print_from_end(struct node *ptr,int node)
+{
+	struct node *temp = ptr;
+	int len = 0;
+	while(temp)
+	{
+		temp=temp->link;
+		len++;
+	}
+	temp=ptr;
+	int i;
+	for(i=1;i<len-node+1;i++)
+	{
+		temp = temp->link;
+	}
+	printf("print_from_end %d\n",temp->data);
+}
+
+
+/*C Program to Find Middle Node of a Linked List*/
+void find_mid_node(struct node *ptr)
+{
+	struct node *temp = ptr;
+	int len = 0;
+	while(temp)
+	{
+		temp=temp->link;
+		len++;
+	}
+	int mid = len/2;
+	temp = ptr;
+	int i=0;
+	for(i=0;i<mid;i++)
+	{
+		temp = temp->link;
+	}
+	printf("mid_node = %d\n",temp->data);	
+}
+
+
+/*Program to Search an Element in Linked List using Loop*/
+int element_search_loop(struct node *ptr,int element)
+{
+	struct node *temp = ptr;
+	int node = 0;
+	while(element!=temp->data)
+	{
+		temp = temp->link;
+		node++;
+	}
+	if(element==temp->data)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+	
+}
+
+/*Program to Search an Element in Linked List using Recursion*/
+int element_search_rec(struct node *ptr,int element)
+{
+	if(ptr->data==element)
+	{
+		return 1;
+	}
+	if(ptr==NULL)
+	{
+		return 0;
+	}
+	return element_search_rec(ptr->link,element);
+}
+
+
 int main()
 {
-	int choice,data,node;
+	int choice,data,node,method;//choice->parent switch case; data->insert,find; node->insertafter,find; method->child switch
 	struct node *head = NULL;
 	while(1)
 	{
-		printf("\n1. insert_at_end\n2. print\n3. delete_at_end\n4. insert_at_front\n5. delete_at_front\n6. Length\n7. length using recursion\n8. insert after given node\n9. Delete node after\n10. Reverse\11. Recursion print\nn0. exit\n");
+		printf("\n1. insert_at_end\n2. print\n3. delete_at_end\n4. insert_at_front\n5. delete_at_front\n6. Length\n7. length using recursion\n8. insert after given node\n9. Delete node after\n10. Reverse\n11. Recursion print\n12. alternate print\n13. print Nth node from end\n14. find mid node\n15. search element\n0. exit\n");
 		scanf("%d",&choice);
 		switch(choice)
 		{
@@ -234,7 +327,6 @@ int main()
 				break;
 			case 10:
 				printf("Reverse\n1. Loop\n2. recursive\n");
-				int method;
 				scanf("%d",&method);
 						struct node *next = NULL;
 						struct node *prev = NULL;
@@ -255,6 +347,34 @@ int main()
 				printf("Print recursion\n");
 				recursion_print(head);
 				break;
+			case 12:
+				printf("Print alternate nodes\n");
+				print_alternate(head);
+				break;
+			case 13:
+				printf("Print nth node from end\n");
+				scanf("%d",&node);
+				print_from_end(head,node);
+				break;
+			case 14:
+				printf("find mid node\n");
+				find_mid_node(head);
+				break;
+			case 15:
+				printf("1. loop\n2. Recursive\n");
+				scanf("%d",&method);
+				printf("Enter the element that need to search\n");
+				scanf("%d",&data);
+				switch(method)
+				{
+					case 1:
+						element_search_loop(head,data)?printf("element is there\n"):printf("Element not there\n");
+						break;
+					case 2:
+						element_search_rec(head,data)?printf("element is there\n"):printf("Element not there\n");
+						break;	
+				}
+				break;		
 			case 0:
 					exit(0);
 			default:
